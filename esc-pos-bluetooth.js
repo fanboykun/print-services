d448c387058ec94683d2ab522859f68aa42394dd
fanboykun <irfanramadhan1812@gmail.com>
2024-01-25 14:20:23 +0700

    change plain html to url

--- esc-pos-bluetooth.js
+++ esc-pos-bluetooth.js
@@ -6,11 +6,13 @@ function handleESC() {
     // let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='data:text/html,<p>Okay</p>'";
     let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='data:text/html,";
     // let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='http://192.168.100.14/bt-xa'";
-    dynHtml += "<h1 style='text-align:center'>PRINTING DYNAMICALLY GENERATED HTML</h1>"
+    dynHtml += <h1 style='text-align:center'>PRINTING DYNAMICALLY GENERATED HTML</h1>
     dynHtml += "'";
+    let urlTo = "print://escpos.org/escpos/bt/print?srcTp=uri&srcObj=html&src='https://loopedlabs.com/web-print/bill.html'"
     try {
         console.log(dynHtml)
-        window.location.href = dynHtml;
+        window.location.href = urlTo;
+        // window.location.href = dynHtml;
     } catch ( err ) {
         alert(err)
     }
