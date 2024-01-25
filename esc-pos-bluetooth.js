3d9b42832c9a9afd0644ef12aeed32946a9d47b7
fanboykun <irfanramadhan1812@gmail.com>
2024-01-25 14:21:38 +0700

    fix type

--- esc-pos-bluetooth.js
+++ esc-pos-bluetooth.js
@@ -6,7 +6,7 @@ function handleESC() {
     // let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='data:text/html,<p>Okay</p>'";
     let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='data:text/html,";
     // let dynHtml = "print://escpos.org/escpos/bt/print/?srcTp=uri&srcObj=html&src='http://192.168.100.14/bt-xa'";
-    dynHtml += <h1 style='text-align:center'>PRINTING DYNAMICALLY GENERATED HTML</h1>
+    dynHtml += "<h1 style='text-align:center'>PRINTING DYNAMICALLY GENERATED HTML</h1>"
     dynHtml += "'";
     let urlTo = "print://escpos.org/escpos/bt/print?srcTp=uri&srcObj=html&src='https://loopedlabs.com/web-print/bill.html'"
     try {
